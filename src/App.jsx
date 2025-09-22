78b94102c4ef8042756cfcc83526311b0ef7d900
Tiago Citrangulo da Silva <tiagocitrangulo256@gmail.com>
2025-09-22 11:40:26 -0300

    sadasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 
 import MarkdownViewer from './MarkdownViewer';
-import { BrowserRouter, Routes, Route  } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home/Home.jsx'
 
 function App() {
-  
 
-  return(
 
-   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/visualizar' element={<MarkdownViewer />}/>
-    </Routes>
-   </BrowserRouter>
+  return (
+    <>
+      asdasdasd
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/visualizar' element={<MarkdownViewer />} />
+        </Routes>
+      </BrowserRouter>
 
-    
+    </>
+
   )
 }
 
